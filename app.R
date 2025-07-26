2dba729fd21997044bbb0e6eca5e1844e030386e
J. Holden <gfs8966@gmail.com>
2025-07-26 07:54:42 -0400

    fixes server timeout

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ library(tidyr)
 library(scales)
 
 color_gradient <- colorRampPalette(c("steelblue", "purple"))(14)
+training_range <- data.frame(date_only = seq(from = as.Date("2025-07-25"), to = as.Date("2025-08-07"), by = "day"))
 
 # Replace this with your own sheet URL
 sheet_url <- "https://docs.google.com/spreadsheets/d/1NNxDkWscfBXmVXzWXZc3NetHMkP1GZteXrSodDInsBE/edit?resourcekey=&gid=1187787320#gid=1187787320"
@@ -39,7 +40,10 @@ server <- function(input, output, session) {
   sheet_data <- read_sheet(sheet_url)
   sheet_data <- sheet_data %>% 
     mutate(date_only = as_date(ymd_hms(Timestamp))) %>% 
-    rename(Player = `Column 1`, Passes = `How many passes today?`, PushUps = `How many pushups today?`)
+    rename(Player = `Column 1`, Passes = `How many passes today?`, PushUps = `How many pushups today?`) %>% 
+    mutate(PushUps = suppressWarnings(as.numeric(unlist(PushUps)))) %>% 
+    mutate(Passes = suppressWarnings(as.numeric(unlist(Passes)))) %>% 
+    mutate(PushUps = replace_na(PushUps, 0), Passes = replace_na(Passes, 0))
   # Barplot output
   output$barplot <- renderPlot({
     # Adjust this depending on your data structure
@@ -61,21 +65,27 @@ server <- function(input, output, session) {
       geom_hline(yintercept = 500, lty = 2)
   })
   
-  team_totals <- sheet_data %>% group_by(date_only) %>% 
+  team_totals <- sheet_data %>%  
+    group_by(date_only) %>%
     summarize(PushUps = sum(PushUps, na.rm = T), Passes = sum(Passes, na.rm = T)) %>% 
-    pivot_longer(names_to = "Exercise", values_to = "Reps", -date_only) %>%
-    mutate(Reps = cumsum(Reps))
-  
+    mutate(PushUps = cumsum(PushUps), Passes = cumsum(Passes)) %>% 
+    left_join(training_range, ., by="date_only") %>% 
+    mutate(Passes = replace_na(Passes, 0), PushUps = replace_na(PushUps, 0)) 
+
   output$pushuptally <- renderPlot({
-    ggplot(filter(team_totals, Exercise == "PushUps"), aes(date_only, Reps)) +
+    ggplot(team_totals, aes(date_only, PushUps)) +
       geom_bar(stat = "identity", fill = "steelblue") + xlab(element_blank()) +
-      theme_minimal() + ggtitle("Push Ups") 
+      theme_minimal() + ggtitle("Push Ups") +
+      ylim(c(0, ifelse(team_pushup_target>max(team_totals$PushUps), team_pushup_target, max(team_totals$PushUps)+100))) +
+      geom_hline(yintercept = team_pushup_target, lty =2, color = 'grey')
     })
   
   output$passtally <- renderPlot({
-    ggplot(filter(team_totals, Exercise == "Passes"), aes(date_only, Reps)) +
-      geom_bar(stat = "identity", fill = "purple") + xlab(element_blank()) +
-      theme_minimal() + ggtitle("Passes") 
+    ggplot(team_totals, aes(date_only, Passes)) +
+      geom_bar(stat = "identity", fill = "steelblue") + xlab(element_blank()) +
+      theme_minimal() + ggtitle("Passes") +
+      ylim(c(0, ifelse(team_pass_target>max(team_totals$Passes), team_pass_target, max(team_totals$Passes)+100))) +
+      geom_hline(yintercept = team_pass_target, lty =2, color = 'grey') 
   })
 }
 
